6b4c566cda86c98f6cfc5393dbc67c80cd8c2e85
Jhonny Mendoza <jhomen11@gmail.com>
2022-06-01 17:18:05 -0400

    menú lateral

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  texto: string;
+  ruta: string
+}
+
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styles: [
   ]
 })
-export class SidemenuComponent implements OnInit {
+export class SidemenuComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  templateMneu: MenuItem[] = [
+    {
+      texto: "Básicos",
+      ruta: "template/basicos"
+    },
+    {
+      texto: "Diámicos",
+      ruta: "template/dinamicos"
+    },
+    {
+      texto: "Switches",
+      ruta: "template/switches"
+    },
+  ]
 
 }
