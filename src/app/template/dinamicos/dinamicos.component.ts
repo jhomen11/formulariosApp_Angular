904c92d061869dee9fc547db4e3cc636ae768b61
Jhonny Mendoza <jhomen11@gmail.com>
2022-06-07 09:01:08 -0400

    Formularios dinamicos, eliminar elementos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit, Output, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+
+interface Persona{
+  nombre: string;
+  favoritos: Favoritos[]
+}
+
+interface Favoritos{
+  id: number;
+  nombre: string
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+
+  persona: Persona = {
+    nombre: 'Jhonny',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear'},
+      { id: 2, nombre: 'Contra'}
+    ]
+  }
 
   @ViewChild('miFormulario') miFormulario!: NgForm
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   nombreValido():boolean{
     return this.miFormulario?.controls['nombre']?.invalid
@@ -23,6 +38,10 @@ export class DinamicosComponent implements OnInit {
 
   guardar(){
     console.log('Formulario enviado')
+  }
+
+  eliminar(id:number){
+    this.persona.favoritos.splice(id, 1)
   }
 
 }
