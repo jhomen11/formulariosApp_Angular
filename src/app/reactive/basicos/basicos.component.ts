ca375aba931c5dcbc0e954eec42235af33044e2c
Jhonny Mendoza <jhomen11@gmail.com>
2022-06-07 11:35:17 -0400

    Formularios Reactivos, básicos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080'),
+  //   precio: new FormControl(1000),
+  //   existencias: new FormControl(5)
+  // })
 
-  ngOnInit(): void {
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: [ , [ Validators.required, Validators.minLength(3) ] ],
+    precio: [ , [ Validators.required, Validators.min(0) ] ],
+    existencias: [ , [ Validators.required, Validators.min(0) ]]
+  })
+
+  constructor( private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      nombre: 'RTX 4080',
+      precio: 1500
+    })
+  }
+
+  validarCampo(campo:string){
+   return this.miFormulario.controls[campo].errors 
+        && this.miFormulario.controls[campo].touched
+  }
+
+  guardar(){
+   if( this.miFormulario.invalid ){
+    this.miFormulario.markAllAsTouched()
+    return
+   }
+   this.miFormulario.reset()
   }
 
 }
